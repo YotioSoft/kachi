ad6ee2ab9d960d159604c79fcc52cc4486387a90
YotioSoft <ytani0323@gmail.com>
2022-11-17 21:46:40 +0900

    get modified datetime from target files

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ mod run;
 fn main() {
     let targets = run::analyzer::analyze("kachifile".to_string()).expect("Failed to analyze file");
 
-    run::run(targets);
+    let target_name = "hello".to_string();
+    let target = targets.into_iter().find(|t| t.target_name == target_name).expect("Failed to find target");
+    run::run(target);
 }
--- src/run.rs
+++ src/run.rs
@@ -1,9 +1,27 @@
+extern crate chrono;
+use chrono::prelude::*;
+use std::fs::{File, Metadata};
 pub mod analyzer;
 
-pub fn run(targets: Vec<analyzer::Target>) {
-    for target in targets {
-        println!("Target: {}", target.target_name);
-        println!("Files: {:?}", target.files);
-        println!("Commands: {:?}", target.commands);
+fn to_datetime(metadata: Metadata) -> chrono::DateTime<chrono::Local> {
+    let datetime: DateTime<Utc> = chrono::DateTime::from(metadata.modified().unwrap());
+    datetime.with_timezone(&chrono::Local)
+}
+
+fn get_file_metadata(filepaths: Vec<String>) -> Result<Vec<Metadata>, std::io::Error> {
+    let mut metadata: Vec<Metadata> = Vec::new();
+    for filepath in filepaths {
+        let file = File::open(filepath).unwrap();
+        metadata.push(file.metadata()?);
+    }
+    Ok(metadata)
+}
+
+pub fn run(target: analyzer::Target) {
+    let metadata = get_file_metadata(target.files);
+    if let Ok(metadata) = metadata {
+        for file in metadata {
+            println!("File modified: {}", to_datetime(file));
+        }
     }
 }
