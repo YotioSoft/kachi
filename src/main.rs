5714daecdb36afa0f87629c747b9591230411112
YotioSoft <ytani0323@gmail.com>
2022-11-22 23:05:24 +0900

    search kachifile in current dir

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 mod run;
+use std::fs;
 
 fn main() {
-    let targets = run::analyzer::analyze("kachifile".to_string()).expect("Failed to analyze file");
-
-    let target_name = "hello".to_string();
-    let target = targets.into_iter().find(|t| t.target_name == target_name).expect("Failed to find target");
-    run::run(target);
+    // kachifileの検索
+    fs::read_dir("./")
+        .unwrap()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.file_name().to_ascii_lowercase().to_str().unwrap() == "kachifile")
+        .for_each(|entry| {
+            // kachifile を実行
+            if entry.file_type().is_ok() {
+                let filepath = entry.path().to_str().unwrap().to_string();
+                println!("kachifile: {}", filepath);
+                let targets = run::analyzer::analyze(filepath).unwrap();
+                let target_name = "hello".to_string();
+                let target = targets.into_iter().find(|t| t.target_name == target_name).expect("Failed to find target");
+                run::run(target);
+            }
+        });
 }
